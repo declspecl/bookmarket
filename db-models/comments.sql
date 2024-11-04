ffafb39a21dfabbf2ac0d0e7cc4c14f9625f294e
BoiGia Phui <162828475+giaant22@users.noreply.github.com>
2024-11-03 22:53:21 -0500

    Add comments DB model (#3)
    * Add comments DB model
    
    * Update comments DB model with correct syntax for SQLite
    
    * Fix the foreign key syntax for creator_id, parent_comment_id, parent_listing_id
    
    ---------
    
    Co-authored-by: BoiGia Phui <boigiaphui@BoiGias-MacBook-Air.local>

---
+++ db-models/comments.sql
@@ -0,0 +1,13 @@
+-- db-models/comments.sql
+
+CREATE TABLE comments (
+    comment_id INTEGER PRIMARY KEY AUTOINCREMENT,                   -- Unique for each comment, auto-incremented
+    parent_comment_id INTEGER,                                      -- ID of the parent comment for replies
+    parent_listing_id INTEGER NOT NULL,                             -- ID of the parent comment listing, required
+    creator_id INTEGER NOT NULL,                                    -- ID of the author, required
+    content TEXT NOT NULL,                                          -- Comment text
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,             -- Timestamp when comment was created
+    FOREIGN KEY (creator_id) REFERENCES users(user_id),
+    FOREIGN KEY (parent_comment_id) REFERENCES comments(comment_id),
+    FOREIGN KEY (parent_listing_id) REFERENCES listings(listing_id)
+);
