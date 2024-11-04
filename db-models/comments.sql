53782bdf42c6e51c506f20849e1eed055bfd65f4
BoiGia Phui <boigiaphui@BoiGias-MacBook-Air.local>
2024-11-03 20:23:11 -0500

    Update comments DB model with correct syntax for SQLite

--- db-models/comments.sql
+++ db-models/comments.sql
@@ -1,9 +1,12 @@
 -- db-models/comments.sql
 
 CREATE TABLE comments (
-    comment_id SERIAL PRIMARY KEY,          -- Unique for each comment
-    parent_comment_id INTEGER,              -- ID of the parent comment for replies
-    author_name TEXT NOT NULL,              -- Author comment's name
-    content TEXT NOT NULL,                  -- Comment text
-    created_at TIMESTAMP DEFAULT NOW()      --Timestamp when comment was created
+    comment_id INTEGER PRIMARY KEY AUTOINCREMENT,                   -- Unique for each comment, auto-incremented
+    parent_comment_id INTEGER,                                      -- ID of the parent comment for replies
+    parent_listing_id INTEGER NOT NULL,                             -- ID of the parent comment listing, required
+    creator_id INTEGER NOT NULL,                                    -- ID of the author, required
+    content TEXT NOT NULL,                                          -- Comment text
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,             -- Timestamp when comment was created
+    FOREIGN KEY (creator_id) REFERENCES creators(id),
+    FOREIGN KEY (parent_comment_id) REFERENCES comments(comment_id)
 );
