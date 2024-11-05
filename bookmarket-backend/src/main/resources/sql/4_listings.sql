03a4022e783a0ab07dd7374dcfe095e8cff70b18
declspecl <gavind2559@gmail.com>
2024-11-05 17:18:07 -0500

    add listings APIs to initializer

--- bookmarket-backend/src/main/resources/sql/4_listings.sql
+++ bookmarket-backend/src/main/resources/sql/4_listings.sql
@@ -1,3 +1,12 @@
 CREATE TABLE IF NOT EXISTS listings (
-    listing_id INTEGER PRIMARY KEY AUTOINCREMENT
+    listing_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author TEXT NOT NULL,
+    title TEXT NOT NULL,
+    price REAL NOT NULL,
+    description TEXT NOT NULL,
+    seller_id INTEGER NOT NULL,
+    class_subject TEXT NOT NULL,
+    condition TEXT NOT NULL,
+    sale_availability TEXT NOT NULL,
+    FOREIGN KEY (seller_id) REFERENCES users(user_id) ON DELETE SET NULL
 );
