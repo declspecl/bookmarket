dfe8496a32f6bd313dba07ea2f7870ad40038319
declspecl <gavind2559@gmail.com>
2024-11-04 23:15:36 -0500

    fix sql styling and wire up with SQLiteInitializer

--- db-models/auth.sql
+++ db-models/auth.sql
@@ -3,20 +3,20 @@
 -- User Table 
 
 CREATE TABLE users (
-    userId INTEGER PRIMARY KEY AUTOINCREMENT,               -- Identifes each user 
-    firstName TEXT NOT NULL,                                -- User's first name
-    lastName TEXT NOT NULL,                                 -- User's Last name
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,              -- Identifes each user 
+    first_name TEXT NOT NULL,                               -- User's first name
+    last_name TEXT NOT NULL,                                -- User's Last name
     email TEXT NOT NULL,                                    -- User's email address 
     password TEXT NOT NULL,                                 -- User's Password
-    createdAt TEXT DEFAULT CURRENT_TIMESTAMP                
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP                
 );
 
 -- Session Table 
 
-CREATE TABLE session (
-    userId INTEGER NOT NULL,                                       -- Links session to user
-    sessionId TEXT PRIMARY KEY,                                    -- Unique session token, UUID format (e.g., `6e8f68bb-e62b-4861-aadb-ea408ce5ae31`)
-    createdAt TEXT DEFAULT CURRENT_TIMESTAMP                       -- Timestamp for when the session was created
-    expiresAt TEXT NOT NULL,                                       -- Timestamp for when the session expires
-    FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
+CREATE TABLE sessions (
+    session_id TEXT NOT NULL PRIMARY KEY,                             -- Unique session token, UUID format (e.g., `6e8f68bb-e62b-4861-aadb-ea408ce5ae31`)
+    user_id INTEGER NOT NULL,                                         -- Links session to user
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP                -- Timestamp for when the session was created
+    expires_at TEXT NOT NULL,                                         -- Timestamp for when the session expires
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
