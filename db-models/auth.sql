419f3e971266637a6e7fefb00f78995022bfd4a1
jlobrien22 <jlobrien@oakland.edu>
2024-11-04 22:37:14 -0500

    Add auth sql db model updates

--- db-models/auth.sql
+++ db-models/auth.sql
@@ -3,20 +3,20 @@
 -- User Table 
 
 CREATE TABLE users (
-    userId INTEGER PRIMARY KEY,                             -- Identifes each user 
+    userId INTEGER PRIMARY KEY AUTOINCREMENT,               -- Identifes each user 
     firstName TEXT NOT NULL,                                -- User's first name
     lastName TEXT NOT NULL,                                 -- User's Last name
     email TEXT NOT NULL,                                    -- User's email address 
     password TEXT NOT NULL,                                 -- User's Password
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP           -- 
+    createdAt TEXT DEFAULT CURRENT_TIMESTAMP                
 );
 
 -- Session Table 
 
 CREATE TABLE session (
-    userId INTEGER NOT NULL,                                            -- Links session to user
-    sessionId TEXT PRIMARY KEY,                                         -- Unique session token, UUID format (e.g., `6e8f68bb-e62b-4861-aadb-ea408ce5ae31`)
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP                       -- Timestamp for when the session was created
-    expiresAt TIMESTAMP NOT NULL,                                       -- Timestamp for when the session expires
+    userId INTEGER NOT NULL,                                       -- Links session to user
+    sessionId TEXT PRIMARY KEY,                                    -- Unique session token, UUID format (e.g., `6e8f68bb-e62b-4861-aadb-ea408ce5ae31`)
+    createdAt TEXT DEFAULT CURRENT_TIMESTAMP                       -- Timestamp for when the session was created
+    expiresAt TEXT NOT NULL,                                       -- Timestamp for when the session expires
     FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
 );
