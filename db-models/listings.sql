9dbff2ead1cd1a49fcfe968ebeaba353b0449dea
bella-c-5 <izabelacamaj@oakland.edu>
2024-11-05 16:45:05 -0500

    listings db model added (#21)
    * Create listings.sql
    
    SQL db model to store book listings:
    - includes key fields to represent important information about each book listing, such as title, author, condition, price, etc.
    
    * Updated listings.sql
    
    SQL db model to store book listings: includes key fields to represent important information about each book listing, such as title, author, condition, price, etc.

---
+++ db-models/listings.sql
@@ -0,0 +1,13 @@
+-- db-models/listings.sql
+CREATE TABLE listings (
+    listings_id INTEGER PRIMARY KEY AUTOINCREMENT,       -- Auto-incrementing ID
+    author TEXT NOT NULL,                       
+    title TEXT NOT NULL,                     
+    price REAL NOT NULL,                            -- Floating-point number
+    description TEXT NOT NULL,                     
+    seller_id INTEGER NOT NULL,                 -- Foreign key to link to the sellers table
+    class_subject TEXT NOT NULL,                        
+    condition TEXT NOT NULL,                    
+    sale_availability TEXT NOT NULL,
+    FOREIGN KEY (seller_id) REFERENCES users(user_id) ON DELETE SET NULL
+);
