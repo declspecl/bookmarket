b4dd6336e241bd21e1e388e35aabc5eb0d248f69
jlobrien22 <jlobrien@oakland.edu>
2024-11-11 08:52:40 -0500

    Implement listingById API

--- bookmarket-backend/src/main/java/edu/oakland/sophomoreproject/controllers/ListingsController.java
+++ bookmarket-backend/src/main/java/edu/oakland/sophomoreproject/controllers/ListingsController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.sql.init.SqlR2dbcScriptDatabaseInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,11 +51,16 @@ public class ListingsController {
 			HttpServletRequest request,
 			@PathVariable("listingId") Integer listingId
 	) throws SQLException {
-		// ... do logic here
+		Listing listing = listingsTableAccessor.getListingById(listingId);
 
 		System.out.println(listingId);
 
-		return ResponseEntity.ok().build();
+		if (lisitng == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		GetAllListingsResponse response = new GetAllListingsResponse(listing);
+
+		return ResponseEntity.ok().body(response);
 	}
 
 	@GetMapping("/api/listings")
