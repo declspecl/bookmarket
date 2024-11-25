ea50b07f24118ec994a14e0866fa4cb56f96d66d
jlobrien22 <156925826+jlobrien22@users.noreply.github.com>
2024-11-25 17:38:57 -0500

    Create login/signup pages. (#35)

--- bookmarket-website/src/pages/LoginPage.tsx
+++ bookmarket-website/src/pages/LoginPage.tsx
@@ -1,12 +1,59 @@
-import { NavBar } from "@/components/NavBar";
 import "./LoginPage.css";
 
+import { NavBar } from "@/components/NavBar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export function LoginPage() {
+    const [inputtedEmail, setInputtedEmail] = useState("");
+    const [inputtedPassword, setInputtedPassword] = useState("");
+
     return (
-        <div>
+        <div className="login-page">
             <NavBar />
 
-            <h1>Login</h1>
+            <h1 className="page-title">Login</h1>
+
+            <div className="flex flex-col gap-6">
+                {/*Email Field*/}
+                <div>
+                    <h2 className="title">Email Address</h2>
+                    <Input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        value={inputtedEmail}
+                        onChange={(e) => setInputtedEmail(e.target.value)}
+                        required
+                    />
+                </div>
+
+                {/*Password Field*/}
+                <div>
+                    <h2 className="title">Password</h2>
+                    <Input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        value={inputtedPassword}
+                        onChange={(e) => setInputtedPassword(e.target.value)}
+                        required
+                    />
+                </div>
+
+                {/*Submit Button*/}
+                <div className="submit-box">
+                    <Button>Login</Button>
+                </div>
+            </div>
+                 {/*Sign-up Prompt*/}
+            <p className="signup-prompt">
+                Don't have an account? <Link to="/signup">Click here</Link> to sign up
+            </p>
         </div>
     );
 }
